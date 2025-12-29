b467db61a523aab0c3eea9537acad005c8ee8487
SurviantLLC <srivantv@surviantllc.com>
2025-12-29 00:06:54 -0800

    fix: whitelist S3/CloudFront images and update walkthrough screenshots

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,22 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'd2bd5h5te3s67r.cloudfront.net',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 's3-us-west-2.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
